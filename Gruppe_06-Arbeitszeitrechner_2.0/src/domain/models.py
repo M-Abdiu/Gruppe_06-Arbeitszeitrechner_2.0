58a0cbac36d6e1fcdc82c28b1e89f7fb480ba65d
Denis Silva Meira <denissilvameira@outlook.com>
2026-04-21 19:27:48 +0200

    .

--- Gruppe_06-Arbeitszeitrechner_2.0/src/domain/models.py
+++ Gruppe_06-Arbeitszeitrechner_2.0/src/domain/models.py
@@ -3,7 +3,7 @@ from sqlmodel import Field, SQLModel
 
 class User(SQLModel, table=True):
     
-    pk_user_id: Optional[int] = Field(default=None, primary_key=True)
+    pk_user_id: int = Field(default=None, primary_key=True)
     username: str
     Vorname: str
     Nachname: str
@@ -14,14 +14,14 @@ class User(SQLModel, table=True):
 
 class Violation (SQLModel, table=True):
     
-    pk_violation_id: Optional[int] = Field(default=None, primary_key=True)
-    fk_TimeEntry_id: Optional[int] = Field(default=None, foreign_key="TimeEntry.pk_TimeEntry_id")
+    pk_violation_id: int = Field(default=None, primary_key=True)
+    fk_TimeEntry_id: int = Field(default=None, foreign_key="TimeEntry.pk_TimeEntry_id")
     type: str
     
 class TimeEntry(SQLModel, table=True):
     
-    pk_TimeEntry_id: Optional[int] = Field(default=None, primary_key=True)
-    fk_user_id: Optional[int] = Field(default=None, foreign_key="user.pk_user_id")
+    pk_TimeEntry_id: int = Field(default=None, primary_key=True)
+    fk_user_id: int = Field(default=None, foreign_key="user.pk_user_id")
     Kalenderwoche: int
     Tag: str
     MorgenBeginn: float
