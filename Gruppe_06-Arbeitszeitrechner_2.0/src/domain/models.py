175c73caffc0de08329b4d2ca2959586d3b3bc34
Denis Silva Meira <denissilvameira@outlook.com>
2026-04-21 18:31:42 +0200

    Model merge and folder structure

---
+++ Gruppe_06-Arbeitszeitrechner_2.0/src/domain/models.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+class User(SQLModel, table=True):
+    
+    pk_user_id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    Vorname: str
+    Nachname: str
+    Email: str
+    Passwort:  str 
+    IsAAdmin: bool
+    
+
+class Violation (SQLModel, table=True):
+    
+    pk_violation_id: Optional[int] = Field(default=None, primary_key=True)
+    fk_TimeEntry_id: Optional[int] = Field(default=None, foreign_key="TimeEntry.pk_TimeEntry_id")
+    type: str
+    
+class TimeEntry(SQLModel, table=True):
+    
+    pk_TimeEntry_id: Optional[int] = Field(default=None, primary_key=True)
+    fk_user_id: Optional[int] = Field(default=None, foreign_key="user.pk_user_id")
+    Kalenderwoche: int
+    Tag: str
+    MorgenBeginn: float
+    MorgenStop: float
+    NachmittagBeginn:  float 
+    NachmittagStop: float    
--- Gruppe_06-Arbeitszeitrechner_2.0/src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from Database import engine, SQLModel
-from sqlmodel import Session
-from sqlmodel import select
-with Session(engine) as session:
-
-    session.commit()
